4c41642b98bf70239b1c5d8ece7185da75f8f914
Aman Ladwa <amanladwa@MacBook-Pro.local>
2020-11-10 15:05:17 +0530

    next

---
+++ app/src/main/java/com/viber/multipleactivitydemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.viber.multipleactivitydemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void goNext(View view){
+        Intent intent = new Intent(getApplicationContext(),SecondActivity.class);
+        intent.putExtra("age",28);
+        startActivity(intent);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
